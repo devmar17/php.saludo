d6da1d85f1983bb5fd0aeb12a0acc90a864967ab
Marvin <mazmac703@gmail.com>
2025-03-31 17:23:36 -0600

    Primer commit - saludo en PHP

---
+++ README.md
@@ -0,0 +1,1 @@
+# saludo-php-con-imagen
---
+++ hola.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola Mundo PHP</title>
+</head>
+<body>
+    <h1>Hola mundo, soy Marvin Moisés Mazariegos Toyom</h1>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.pinterest.com%2Fpin%2F351562314665104460%2F&psig=AOvVaw1TUkaxjNsAyjfcgTry_oT0&ust=1743540843067000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjmvsOotYwDFQAAAAAdAAAAABAK
+" alt="Hola" width="300">
+</body>
+</html>
